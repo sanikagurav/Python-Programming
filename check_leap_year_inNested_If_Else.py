c279711e5b96eb95b4acfef94766e1a49a46dc45
sanikagurav <143166530+sanikagurav@users.noreply.github.com>
2023-08-27 22:21:34 +0530

    check_leap_year_inNested_If_Else.py

---
+++ check_leap_year_inNested_If_Else.py
@@ -0,0 +1,13 @@
+#check enter year is leap year or not
+year = int(input("Enter a year : "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year")
+        else:
+            print("Not a leap year")
+    else:
+        print("Leap year")
+else:
+    print("Not a leap year")
